9a30a110eb9b5a5b702f88f3f02af755b012b406
dasdevidutta3@gmail.com <dasdevidutta3@gmail.com>
2026-07-02 06:20:13 +0530

    committed

---
+++ Topic-1/cu_2_T_1_verify_installation.py
@@ -0,0 +1,9 @@
+# You must need to verify wheather the numpy is installed in your system
+# Steps
+# 1.Open Terminal (ctrl+shift+~)
+# 2.check py version(3.14.4)
+#pip install numpy then hit enter 
+# we verify the installation if the line-8 execute then our numpy installed successfully...
+
+import numpy as np
+print("Hii CodeUdaan")
