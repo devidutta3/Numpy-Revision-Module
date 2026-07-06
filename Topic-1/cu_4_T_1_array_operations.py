a030bb0101dd79abb43125996cd9ccfd59f8f589
dasdevidutta3@gmail.com <dasdevidutta3@gmail.com>
2026-07-07 05:18:50 +0530

    committed

--- Topic-1/cu_4_T_1_array_operations.py
+++ Topic-1/cu_4_T_1_array_operations.py
@@ -4,13 +4,16 @@ import numpy as np
 
 def addition(array_1,array_2):
     array_sum=np.add(array_1,array_2)
-    return array_sum
+    array_sub=np.subtract(array_1,array_2)
+    array_multi=np.multiply(array_1,array_2)
+    array_div=np.divide(array_1,array_2)
+    return array_sum , array_sub , array_multi , array_div
 
 
 try:
     array_1=np.array([1,11])
     array_2=np.array([1,15])
     result=addition(array_1,array_2)
-    print(result)
+    print(result,end="\n")
 except TypeError:
     print("Try Again.....")
